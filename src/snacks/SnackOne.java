bed324f5e3dfac1bab8c8ea593c8027fb50dba9c
gioanbatan <giovannibassani@hotmail.com>
2023-03-09 14:19:58 +0100

    first commit

---
+++ src/snacks/SnackOne.java
@@ -0,0 +1,15 @@
+package snacks;
+
+import java.util.Scanner;
+
+public class SnackOne {
+    public static void main(String[] args) {
+        /*
+        Snack 1 Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+         */
+
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
