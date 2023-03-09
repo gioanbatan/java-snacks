db697ad6eeedd9bb1cc3b60c86a9e5df89ce437a
gioanbatan <giovannibassani@hotmail.com>
2023-03-09 14:28:49 +0100

    Add: Snack 1

--- src/snacks/SnackOne.java
+++ src/snacks/SnackOne.java
@@ -8,8 +8,16 @@ public class SnackOne {
         Snack 1 Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
          */
 
+        int userNumber;
         Scanner scan = new Scanner(System.in);
 
+        System.out.print("Inserisci un numero intero: ");
+        userNumber = Integer.parseInt(scan.nextLine());
 
+        if (userNumber%2 == 0) {
+            System.out.println(userNumber);
+        } else {
+            System.out.println(userNumber + 1);
+        }
     }
 }
